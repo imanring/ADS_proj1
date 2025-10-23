8b8de58389d007c072d3cec20233b800a26b8fb1
Isaac Manring <isaacamanring@gmail.com>
2025-10-23 19:06:40 +0000

    frankenstein

--- src/min_heap.py
+++ src/min_heap.py
@@ -1,39 +1,108 @@
+class MinHeapNode:
+    def __init__(self, key, payload, idx):
+        self.key = key
+        self.payload = payload
+        self.idx = idx
+    def __lt__(self, other):
+        return self.key < other.key
+    def __le__(self, other):
+        return self.key <= other.key
+    def __eq__(self, other):
+        return self.key == other.key
+    def __gt__(self, other):
+        return self.key > other.key
+    def __ge__(self, other):
+        return self.key >= other.key
+    def __ne__(self, other):
+        return self.key != other.key
+
 class MinHeap:
     def __init__(self):
         self.nodes = []
 
     def get_min(self):
         if len(self.nodes) == 0:
-            return -1
+            return MinHeapNode((1e32,1e32), None, -1)
         return self.nodes[0]
 
     def pop_min(self):
         if len(self.nodes) == 0:
-            return -1
+            return MinHeapNode((1e32,1e32), None, -1)
         elif len(self.nodes) == 1:
             m = self.nodes[0]
             self.nodes = []
             return m
         else:
             m = self.nodes[0]
-            l = self.nodes[-1]
+            e = self.nodes[-1]
             self.nodes = self.nodes[:-1]
-            self.nodes[0] = l
+            e.idx = 0
+            self.nodes[0] = e
             # perculate down
-            p = 0
-            l = 2*p + 1
-            r = 2*p + 2
+            self._bubble_down(0)
+            return m
+
+    def insert(self, key, payload):
+        new_node = MinHeapNode(key, payload, len(self.nodes))
+        self.nodes.append(new_node)
+        self._bubble_up(len(self.nodes) - 1)
+        return new_node
+    
+    def arbitrary_delete(self, index):
+        if index >= len(self.nodes) or index < 0:
+            return -1
+        elif index == len(self.nodes) - 1:
+            self.nodes = self.nodes[:-1]
+            return
+        elif index == 0:
+            self.pop_min()
+            return
+        else:
+            e = self.nodes[-1]
+            e.idx = index
+            self.nodes = self.nodes[:-1]
+            self.nodes[index] = e
+            if self.nodes[(index-1)//2] > self.nodes[index]:
+                self._bubble_up(index)
+                return
+            else:
+                self._bubble_down(index)
+                return
+
+    def pop_range(self, lteq):
+         # lteq must be the same data structure as a key.
+        results = []
+        while len(self.nodes) != 0 and lteq >= self.nodes[0].key:
+            results.append(self.pop_min())
+        return results
+
+    def change_key(self, index, new_key):
+        if index >= len(self.nodes) or index < 0:
+            return -1
+        old_key = self.nodes[index].key
+        self.nodes[index].key = new_key
+        if new_key < old_key:
+            self._bubble_up(index)
+        else:
+            self._bubble_down(index)
+
+    def _bubble_down(self, index):
+            p = index # parent
+            l = 2*p + 1 # left
+            r = 2*p + 2 # right
             while r < len(self.nodes) and self.nodes[p] > min(self.nodes[l], self.nodes[r]):
+                t = self.nodes[p]
                 if self.nodes[l] > self.nodes[r]:
-                    t = self.nodes[p]
                     self.nodes[p] = self.nodes[r]
                     self.nodes[r] = t
+                    self.nodes[r].idx = r
                     p = r
                 else:
-                    t = self.nodes[p]
                     self.nodes[p] = self.nodes[l]
                     self.nodes[l] = t
+                    self.nodes[l].idx = l
                     p = l
+                self.nodes[p].idx = p
                 l = 2*p + 1
                 r = 2*p + 2
             # accounting for odd case where there is not a left and right
@@ -41,16 +110,18 @@ class MinHeap:
                 # swap values
                 t = self.nodes[-1]
                 self.nodes[-1] = self.nodes[(len(self.nodes) - 2) // 2]
+                self.nodes[-1].idx = len(self.nodes) - 1
                 self.nodes[(len(self.nodes) - 2) // 2] = t
-            return m
+                self.nodes[(len(self.nodes) - 2) // 2].idx = (len(self.nodes) - 2) // 2
 
-    def insert(self, x):
-        self.nodes.append(x)
-        i = len(self.nodes)-1
+    def _bubble_up(self, index):
+        i = index
         p = (i - 1) // 2
         while i > 0 and self.nodes[p] > self.nodes[i]:
             t = self.nodes[p]
             self.nodes[p] = self.nodes[i]
+            self.nodes[p].idx = p
             self.nodes[i] = t
+            self.nodes[i].idx = i
             i = p
             p = (i - 1) // 2
--- src/scheduler.py
+++ src/scheduler.py
@@ -1,5 +1,6 @@
 from min_heap import MinHeap
-from max_pairing_heap import PairingHeap
+from pairing_heap import PairingHeap
+import copy
 
 class Flight:
     def __init__(self, flightID, airlineID, submitTime, priority, duration):
@@ -8,19 +9,22 @@ class Flight:
         self.submitTime = submitTime
         self.priority = priority
         self.duration = duration
-        self.statue = "PENDING" # PENDING, SCHEDULED, COMPLETED
+        self.state = "PENDING" # PENDING, SCHEDULED, INPROGRESS, COMPLETED
         self.startTime = -1 
         self.ETA = -1
+        self.runwayID = -1
 
 
 class Scheduler:
     def __init__(self, n):
+        # Pending and scheduled flights
         self.pending_flights = None # PairingHeap
         self.runway_pool = MinHeap()
+        # only flights in progress
         self.active_flights = {} # flightID: Flight
         self.time_table = MinHeap() # ((eta, flightID), (runwayID))
         self.airline_index = {}#airlineID: [flightID]
-        self.handles = {}#flightID: PairingHeap
+        self.handles = {}#flightID: (PairingHeap, MinHeapNode)
         
         if n <= 0:
             print("Invalid input. Please provide a valid number of runways.")
@@ -32,92 +36,124 @@ class Scheduler:
             print(f"{n} runways are now available")
 
     def settle(self):
-        while self.time_table.get_min()[0][0] >= self.currentTime:
-            key, payload = self.time_table.pop_min()
-            self.active_flights[key[1]].state = "COMPLETED"
+        while self.time_table.get_min().key[0] <= self.currentTime:
+            node = self.time_table.pop_min()
+            if node.key[1] in self.active_flights:
+                self.active_flights[node.key[1]].state = "COMPLETED"
             # possibly remove from other data structures
-            self.active_flights[key[1]] = None
-            print(f"Flight {key[1]} has complete at time {self.currentTime}")
+            self.active_flights[node.key[1]] = None
+            print(f"Flight {node.key[1]} has complete at time {self.currentTime}")
         # promotion of pending flights to active flights if possible
+        while self.pending_flights is not None and self.pending_flights.payload.startTime <= self.currentTime:
+            # can no longer be rescheduled
+            rslt, self.pending_flights = self.pending_flights.pop_max()
+            rslt[1].state = "INPROGRESS"
+            self.active_flights[rslt[1].flightID] = rslt[1]
+        new_runway_pool = MinHeap()
+        runwayIDs = set(range(1, self.numRunways + 1))
+        for key, value in self.active_flights.items():  # new runway heap
+            new_runway_pool.insert((value.startTime + value.duration, value.runwayID), (value.runwayID, value.startTime + value.duration))
+            runwayIDs.remove(value.runwayID)
+        for rid in runwayIDs:
+            new_runway_pool.insert((self.currentTime, rid), (rid, self.currentTime))
+        return new_runway_pool
 
-    
-
-    def reschedule(self):
+            
+    def reschedule(self, new_runway_pool):
         # pop all flights from pending and push into new pending heap
         new_pending = None
-        # create new next available runway heap based on old one.
-        new_runway_pool = self.runway_pool.copy()
+        self.runway_pool = copy.deepcopy(new_runway_pool)
         # schedule all pending flights
         while self.pending_flights is not None:
             # get next available runway
-            runwayID, nextFreeTime = self.new_runway_pool.pop_min()
+            mhn = self.runway_pool.pop_min()
+            runwayID, nextFreeTime = mhn.payload
             # get highest priority flight
-            key, flight = self.pending_flights.pop_max()
+            flight, self.pending_flights = self.pending_flights.pop_max()
 
             start_time = self.currentTime if self.currentTime > nextFreeTime else nextFreeTime
-            eta = start_time + flight.duration
-            flight.ETA = eta
-            flight.state = "SCHEDULED"
-            flight.startTime = start_time
-            print(f"Flight {flight.flightID} scheduled on Runway {runwayID} - ETA: {eta}")
+            eta = start_time + flight[1].duration
+            flight[1].ETA = eta
+            old_state = flight[1].state
+            flight[1].state = "SCHEDULED"
+            flight[1].startTime = start_time
+            flight[1].runwayID = runwayID
+            print(f"Flight {flight[1].flightID} scheduled on Runway {runwayID} - ETA: {eta}")
+
             # update data structures
-            self.time_table.insert((eta, flight.flightID), runwayID)
-            self.new_runway_pool.insert((eta, runwayID), (runwayID, eta))
-            self.active_flights[flight.flightID] = flight
-            # add to new pending heap with updated key
-            new_fligt = PairingHeap(key, flight)
-            if new_pending is None:
-                new_pending = new_fligt
+            if old_state == "PENDING":
+                node = self.time_table.insert((eta, flight[1].flightID), runwayID)
             else:
-                new_pending = new_pending.meld(new_fligt)
-            self.handles[flight.flightID] = new_fligt
+                node = self.handles[flight[1].flightID][1]
+                self.time_table.change_key(node.idx, (eta, flight[1].flightID))
+            # update runway pool with new next free time
+            self.runway_pool.insert((eta, runwayID), (runwayID, eta))
+            # add to new pending heap with updated key
+            new_flight = PairingHeap(flight[0], flight[1])
+            if new_pending is None:
+                new_pending = new_flight
+            else:
+                new_pending = new_pending.meld(new_flight)
+            self.handles[flight[1].flightID] = (new_flight, node)
         self.pending_flights = new_pending
 
     
     def submitFlight(self, flightID, airlineID, submitTime, priority, duration):
-        # TODO: settle
-        # TODO: reschedule
+        # settle
+        # reschedule
         self.currentTime = submitTime
+        rt = self.settle()
+        self.reschedule(rt)
+
         if flightID in self.handles:
             print("Duplicate flight")
         else:
             # new flight
             f = Flight(flightID, airlineID, submitTime, priority, duration)
             new_flight = PairingHeap((priority, -submitTime, -flightID), f)
-            self.handles[flightID] = new_flight
+
+            self.handles[flightID] = (new_flight, None)
             if self.pending_flights is not None:
                 self.pending_flights.meld(new_flight)
             else:
                 self.pending_flights = new_flight
-            # TODO: reschedule
-            print(f"Flight {flightID} scheduled - ETA: {self.handles[flightID].payload[5]}")
+            # reschedule
+            self.reschedule(rt)
+            print(f"Flight {flightID} scheduled - ETA: {self.handles[flightID][0].payload.ETA}")
             # TODO: print updated ETAs
 
     
     def cancelFlight(self, flightID, currentTime):
-        # TODO: settle, reschedule
+        # settle, reschedule
         self.currentTime = currentTime
+        rt = self.settle()
+        self.reschedule(rt)
+        
         if flightID not in self.handles:
             print("Flight does not exist.")
         elif flightID in self.active_flights:
             print(f"Cannot cancel: Flight {flightID} has already departed.")
         else:
-            self.pending_flights.remove(self.handles[flightID])
+            self.pending_flights.arbitrary_delete(self.handles[flightID][0])
             # shouldn't be in active_flights
-            self.time_table.arbitrary_remove((self.handles[flightID].payload.eta, flightID))
+            self.time_table.arbitrary_delete(self.handles[flightID][1].idx)
             # TODO: inefficient method of removing from airline_index
-            self.airline_index[self.handles[flightID].payload.airlineID] = [c for c in self.airline_index[self.handles[flightID].payload.airlineID] if c != self.handles[flightID].payload.flightID]
+            self.airline_index[self.handles[flightID][0].payload.airlineID] = [c for c in self.airline_index[self.handles[flightID][0].payload.airlineID] if c != self.handles[flightID][0].payload.flightID]
             self.handles[flightID] = None
-            
-            # TODO: reschedule
+
+            # reschedule
+            self.reschedule(rt)
             print(f"Flight {flightID} has been canceled.")
 
             # TODO: print updated ETAs
 
 
     def reprioritize(self, flightID, currentTime, newPriority):
-        # TODO: settle, reschedule
+        # settle, reschedule
         self.currentTime = currentTime
+        rt = self.settle()
+        self.reschedule(rt)
+
         if flightID not in self.handles:
             print("Flight does not exist.")
         elif flightID in self.active_flights:
@@ -125,31 +161,41 @@ class Scheduler:
         else:
             # TODO: what if newPriority < oldPriority
             self.pending_flights.increase_key(self.handles[flightID], newPriority)
-            # TODO: reschedule
+            # reschedule
+            self.reschedule(rt)
             print(f"Priority of Flight {flightID} has been updated to {newPriority}")
             # TODO: print updated ETAs
 
 
     def addRunways(self, count, currentTime):
-        # TODO: settle, reschedule
+        # settle, reschedule
         self.currentTime = currentTime
+        rt = self.settle()
+        self.reschedule(rt)
+
         if count <= 0:
             print("Invalid input. Please provide a valid number of runways.")
         else:
             for i in range(self.numRunways + 1,self.numRunways + 1 + count):
-                self.runway_pool.insert((currentTime,i), (i,currentTime))
-            # TODO: reschedule
+                rt.insert((currentTime,i), (i,currentTime))
+            # reschedule
+            self.reschedule(rt)
+            self.numRunways += count
             print(f"Additional {count} runways are now available")
             # TODO: print updated ETAs
 
     def groundHold(self, airlineLow, airlineHigh, currentTime):
-        # TODO: settle, reschedule
+        # settle, reschedule
         self.currentTime = currentTime
+        rt = self.settle()
+        self.reschedule(rt)
+
         if airlineHigh < airlineLow:
             print("Invalid input. Please provide a valid airline range.")
         else:
             # TODO: remove flights from data structures
-            # TODO: reschedule
+            # reschedule
+            self.reschedule(rt)
             print(f"GroundHold applied to airlines[{airlineLow},{airlineHigh}].")
             # TODO: print updated ETAs
 
@@ -163,17 +209,22 @@ class Scheduler:
             print("No active flights")
 
     def printSchedule(self, t1, t2):
+        # get all scheduled flights
+        #self.pending_flights.dfs_list()
         flights = [f for f in self.active_flights.values() if f.state == "SCHEDULED" and f.startTime < self.currentTime and t1 <= f.ETA and t2 >= f.ETA]
         for f in sorted(flights, key=lambda f: (f.ETA, f.flightID)):
-            print(f"[{f.fligtID}]")
+            print(f"[{f.flightID}]")
         if len(flights) == 0:
             print("There are no flights in that time period")
 
 
     def tick(self, t):
         self.currentTime = t
-        # TODO: settle, reschedule
+        # settle, reschedule
+        rt = self.settle()
+        self.reschedule(rt)
         # TODO: print updated ETAs
+        
 
     def parseCommand(self, command):
         cmd = command.split()
